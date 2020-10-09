db2fcca5adb3ae2e41ef3cd4e6c285e86a0f0ab5
Waseemq1 <43473728+Waseemq1@users.noreply.github.com>
2020-10-10 02:43:29 +0300

    Area of a circle

---
+++ Area_Of_Circle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 01:44:57 2020
+
+@author: hp
+"""
+
+import math 
+
+Rad = int(input("What's your radius? \n\t >>>   "))
+
+Area_Of_Circ = (Rad**2)*(math.pi)
+
+print("your area is " , Area_Of_Circ)
