23731291d785a3113b8a59ebd068cdf0ffa1117c
Waseemq1 <43473728+Waseemq1@users.noreply.github.com>
2020-10-10 14:38:27 +0300

    Multiply Nums

---
+++ product of two numbers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 02:14:39 2020
+
+@author: hp
+"""
+
+Num1 = int(input("Give the first number \n\t >>> "))
+
+Num2 = int(input("Give me the second number \n\t >>> "))
+
+print("The product of the two numbers is " + str(Num1 * Num2))
