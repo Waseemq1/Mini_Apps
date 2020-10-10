fae40193be220cc883477966eaca734945bf0d13
Waseemq1 <43473728+Waseemq1@users.noreply.github.com>
2020-10-10 14:37:56 +0300

    Adds two numbers

---
+++ Sum of two numbers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 02:05:41 2020
+
+@author: hp
+"""
+
+Num1 = int(input("Give the first number \n\t >>> "))
+
+Num2 = int(input("Give me the second number \n\t >>> "))
+
+print("The sum is " + str(Num1 + Num2))
