e83694c259a1b085b7e239683e9a199e7d5d9db8
Waseemq1 <43473728+Waseemq1@users.noreply.github.com>
2020-10-10 14:39:26 +0300

    Temp converter

---
+++ Feh to Cel.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 01:54:53 2020
+
+@author: hp
+"""
+
+Feh = float(input("What's the degree in Fahrenheit?  \n\t >>>"))
+
+Cel = (Feh-32)/1.8
+
+print("Your degree Celsius is " , Cel)
